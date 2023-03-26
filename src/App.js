eda4ca01aa2c0d1b732f9e372d9343e48b944142
Jiteshsharma01 <jitesh171099@gmail.com>
2023-03-26 10:51:16 +0530

    @ecommerce-app @initial files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import Home from './Pages/HomePage/Home';
+import ProductDetail from './Pages/ProductDetail/ProductDetail'
+import ProductForm from './Components/ProductForm/ProductForm';
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/Home" element={<Home/>} />
+      <Route path="/create-product" element={<ProductForm/>} />
+      <Route path="/update-product/:id" element={<ProductForm/>} />
+      <Route path="/product/:id" element={<ProductDetail/>} />
+      <Route path="*" element={<div>
+        <h1>404 PAGE NOT FOUND</h1>
+      </div>} />
+    </Routes>
+   </BrowserRouter>
   );
 }
 
